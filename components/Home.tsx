cbf8548be8ec302956add4b7c568e6c74f6121b4
Hardi99 <harditabuna@gmail.com>
2024-04-11 11:27:20 +0200

    advanced style #home

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <section id="home">
             <div>
-                <h1>Bienvenue chez Café Neko</h1>
+                <h1>Bienvenue<br /> chez Café Neko</h1>
                 <h4>L'endroit où le café rencontre les ronronnements</h4>
                 <p>Bienvenue dans notre univers où chaque tasse de café est accompagnée d'une dose d'amour félin.</p>
                 <button>Nous contacter</button>
